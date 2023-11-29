15a53578e1b64686398648081725b40f519587d5
Miško <125220165+Infy42@users.noreply.github.com>
2023-11-29 16:50:14 +0100

    bubble sort

---
+++ bubble-sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace bubble_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] unsortedList = { 4, 2, 7, 1, 5, 3, 9, 6 };
+
+            int temp;
+
+            Console.WriteLine("Before Bubble Sort");
+
+            for (int index = 0; index < unsortedList.Length; index++)
+            {
+                Console.Write(unsortedList[index] + " ");
+            }
+
+            for (int i = 0; i < unsortedList.Length - 1; i++)
+            {
+                for (int j = 0; j < unsortedList.Length - (i + 1); j++)
+                {
+                    if (unsortedList[j] > unsortedList[j + 1])
+                    {
+                        temp = unsortedList[j + 1];
+                        unsortedList[j + 1] = unsortedList[j];
+                        unsortedList[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nAfter Bubble Sort");
+
+            for (int index = 0; index < unsortedList.Length; index++)
+            {
+                Console.Write(unsortedList[index] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
